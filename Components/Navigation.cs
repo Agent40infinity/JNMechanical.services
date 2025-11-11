901b9126836bce6b4bee16c25dab1ddd9cdace22
Aiden Nathan <agent9@y7mail.com>
2025-11-12 02:12:47 +1100

    Restarting from scratch

---
+++ Components/Navigation.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace aiden.fyi.Components
+{
+    public static class Navigation
+    {
+        public const int duration = 1000;
+
+        public static async Task NavigateTo(NavigationManager navManager, IJSRuntime js, string page)
+        {
+            page = StringCorrection(page);
+
+            var forceRefresh = navManager.ToAbsoluteUri(page).AbsoluteUri == navManager.Uri;
+
+            await js.InvokeVoidAsync("Unloaded", duration);
+            await Task.Delay(duration);
+
+            navManager.NavigateTo(page, forceRefresh);
+        }
+
+        private static string StringCorrection(string title)
+        {
+            return title.Replace(' ', '-');
+        }
+    }
+}
