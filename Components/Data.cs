eeb672480a748ec84da51186a64fe49d94e7a9b5
Aiden Nathan <agent9@y7mail.com>
2025-11-19 03:12:38 +1100

    Services data additions in MainLayout

---
+++ Components/Data.cs
@@ -0,0 +1,7 @@
+﻿namespace jnmechanical.services.Components
+{
+    public class Data
+    {
+        public static Dictionary<string, List<string>> Services = new Dictionary<string, List<string>>();
+    }
+}
