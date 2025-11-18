9f400e224759cbfaeb9c585567af17567c488a40
Aiden Nathan <agent9@y7mail.com>
2025-11-19 03:13:06 +1100

    Inquiry Setup Basics

---
+++ Components/InquiryForm.cs
@@ -0,0 +1,13 @@
+﻿namespace jnmechanical.services.Components
+{
+    public class InquiryForm
+    {
+        public string firstName = "";
+        public string lastName = "";
+        public string email = "";
+        public string phone = "";
+        public string inquiry = "";
+        public string rego = "";
+        public string model = "";
+    }
+}
